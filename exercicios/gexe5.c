ed86d9ef28728086863f5f838ffc32023d5ba1d5
maike <164254808+josmke@users.noreply.github.com>
2025-10-15 19:49:14 +0000

    Continuação

--- exercicios/gexe5.c
+++ exercicios/gexe5.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
+    srand(time(NULL));
     char jogadores[5][50];
-    int pontuacoes[4];
+    int pontuacoes[5];
 
     printf("Insira 5 nomes de jogadores:\n");
     for (int i = 0; i < 5; i++){
         scanf("%s", jogadores[i]);
     }
+    printf("\n"); // quebra de linha
+    for (int i = 0; i < 5; i++){
+        pontuacoes[i] = rand() % 101;       
+    }
 
-    printf("%s\n", jogadores); //um unico print f não imprime todos os elementos do vetor. é necessario usar um loop 
-
+    for (int i = 0; i < 5; i++){
+        printf("Jogador: %s\nPontuacao: %d\n", jogadores[i],pontuacoes[i]); //um unico print f não imprime todos os elementos do vetor. é necessario usar um loop 
+    }
+    
     
     return 0;
 }
